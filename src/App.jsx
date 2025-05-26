2b5e8199c2eee54e8525d4b6b1fad911801aae32
ADudhe01 <atharva.dudhe@gmail.com>
2025-05-26 11:14:38 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ToDo from "./components/ToDo";
+import InlineComponent from "./components/InlineComponent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ToDo /> */}
+      <InlineComponent />
+    </div>
+  );
+}
+
+export default App;
